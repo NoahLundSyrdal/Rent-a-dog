abbd50443d97acd2fbf3e7cfa67fce1638e9bc2b
Marcus Ljungmann Jakobsen <marcus@Marcuss-MacBook-Pro-2.local>
2022-11-18 16:32:08 +0100

    bark fix

--- P3_G2/bark.js
+++ P3_G2/bark.js
@@ -2,7 +2,7 @@ const bark = new Audio("../sound/bark.mp3")
 
 // WHEN PRESSED SEND-BUTTON, TEXTFIELD IS RESET AND BARKNOISE IS PLAYED
 function sendCont() {
+    bark.play()
     confirm('Thank you for contacting!')
     document.getElementById("contacttext").value = ""
-    bark.play()
 }
